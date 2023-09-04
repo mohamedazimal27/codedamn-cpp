273a3201a3aaaa5f6ce6d76ea1b65c090ff7f569
mohamedazimal27 <mohamedazimal27@gmail.com>
2023-09-04 11:56:45 +0800

    update on conditions

---
+++ C++ syntax and structure/5_If-Else-Statements.cpp
@@ -0,0 +1,27 @@
+/*
+If-Else Statements
+Write a program that takes two integers as input from the user and performs the following operations:
+
+If both integers are positive, output their sum and product.
+If both integers are negative, output their difference and quotient.
+If one integer is positive and the other is negative, output their product and quotient.
+If either of the integers is zero, output 0.
+Output only the result (For Example, only sum and product for first case, or '0' without quotes for last case, on newline) and nothing else.
+*/
+// Lab Template
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+    int x,y;
+    cin >> x;
+    cin >> y;
+    if (x > 0 && y > 0)
+    {
+        
+    }
+    return 0;
+}
