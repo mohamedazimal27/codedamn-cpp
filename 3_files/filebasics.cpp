a2d8b3b9097d3d27aac54029aa6f89fbb13be9da
mohamedazimal27 <mohamedazimal27@gmail.com>
2023-09-20 20:41:14 +0800

    added files

---
+++ 3_files/filebasics.cpp
@@ -0,0 +1,13 @@
+// basic file operations
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+  ofstream myfile;
+  myfile.open ("example.txt");
+  myfile << "Writing this to a file.\n";
+  myfile << "I'm a developer.\n";
+  myfile.close();
+  return 0;
+}
