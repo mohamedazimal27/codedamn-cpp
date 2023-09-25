6f4df9a467f83db07fa8a4a2a571d6dbc4dd1e7f
moahmedazimal27 <mohamedazimal27@gmail.com>
2023-09-25 14:14:17 +0800

    added: hacker rank practice

---
+++ 5-Mini-Projects/7-Rock-Paper-Scissors.cpp
@@ -0,0 +1,18 @@
+/*
+Rock, Paper, Scissors
+Create a program that allows the user to play a game of Rock, Paper, Scissors against the computer. 
+The program should prompt the user to input their choice of Rock, Paper, or Scissors, generate a random choice for the computer, 
+and determine the winner based on the rules of the game.
+*/
+
+
+
+#include <iostream>
+#include <random>
+#include <ctime>
+
+int main()
+{
+    
+    return 0;
+}
