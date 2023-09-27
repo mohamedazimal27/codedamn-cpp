1a31328b90a9e3225589a113543c9612845f4d82
moahmedazimal27 <mohamedazimal27@gmail.com>
2023-09-27 18:31:54 +0800

    added: lower bound stl

---
+++ 6-Practice/5-STL/1-Lower-Bound-STL.cpp
@@ -0,0 +1,82 @@
+/*
+You are given  integers in sorted order. Also, you are given  queries.
+In each query, you will be given an integer and you have to tell whether that integer is present in the array.
+If so, you have to tell at which index it is present and if it is not present, you have to tell the index at which
+the smallest integer that is just greater than the given number is present.
+
+Lower bound is a function that can be used with a sorted vector. Learn how to use lower bound to solve this problem by clicking here.
+
+Input Format
+
+The first line of the input contains the number of integers . The next line contains  integers in sorted order. The next line contains ,
+the number of queries. Then  lines follow each containing a single integer .
+
+Note: If the same number is present multiple times, you have to print the first index at which it occurs.
+Also, the input is such that you always have an answer for each query.
+
+Constraints
+
+
+,where  is  element in the array.
+
+
+Output Format
+
+For each query you have to print "Yes" (without the quotes) if the number is present and at which index(1-based)
+it is present separated by a space.
+
+If the number is not present you have to print "No" (without the quotes) followed by the index of the next smallest number just
+greater than that number.
+
+You have to output each query in a new line.
+
+Sample Input
+
+ 8
+ 1 1 2 2 6 9 9 15
+ 4
+ 1
+ 4
+ 9
+ 15
+Sample Output
+
+ Yes 1
+ No 5
+ Yes 6
+ Yes 8
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    int q;
+    cin >> q;
+
+    for (int i = 0; i < q; i++) {
+        int qs;
+        cin >> qs;
+
+        vector<int>::iterator it = lower_bound(v.begin(), v.end(), qs);
+
+        if (*it == qs) {
+            int index = distance(v.begin(), it) + 1; // 1-based index
+            cout << "Yes " << index << endl;
+        } else {
+            int index = distance(v.begin(), it) + 1; // 1-based index of the next smallest number
+            cout << "No " << index << endl;
+        }
+    }
+
+    return 0;
+}
